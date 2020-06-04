99acef8715c050f841eb6ab62d3ddf4a319c7c1e
sarafotros <sarafotros@gmail.com>
2020-06-04 20:09:09 +0100

    action store reduce

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import configureStore from "./store/configureStore";
-import { loadBugs, assignBugToUser } from "./store/bugs";
+// import configureStore from "./store/configureStore";
+// import { loadBugs, assignBugToUser } from "./store/bugs";
 
-const store = configureStore();
+// const store = configureStore();
 
 // UI Layer
-store.dispatch(loadBugs());
+// store.dispatch(loadBugs());
 
-setTimeout(() => store.dispatch(assignBugToUser(1, 5)), 2000);
+// setTimeout(() => store.dispatch(assignBugToUser(1, 5)), 2000);
+
+import store from './store';
+import { bugAdded } from './actions';
+
+const unsubscribe = store.subscribe(() => {
+	console.log('store changed', store.getState());
+});
+
+store.dispatch( bugAdded('Bug1'))
+
+unsubscribe();
+
+store.dispatch({
+	type: actions.BUG_REMOVED,
+	payload: {
+		id: 1,
+	},
+});
+
+console.log('store changed', store.getState());
